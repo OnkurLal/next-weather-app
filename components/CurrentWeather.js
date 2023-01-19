8eb9e5467e17f10bcbb2024c5ea254227bf1e3a8
Onkur Lal <onkur24lal@gmail.com>
2023-01-19 15:49:41 -0500

    added styling to current weather component

--- components/CurrentWeather.js
+++ components/CurrentWeather.js
@@ -10,20 +10,44 @@ export default function CurrentWeather({
 }) {
   return (
     <>
-      <div className="card text-black w-75">
-        <div className="card-body p-3">
-          <h1 className="card-title">
-            {city.name} ({city.country})
-          </h1>
-          <h2>Current Temperature {currentTemp}°F</h2>
-          <h2>
-            High {high}°F Low {low}°F
-          </h2>
-          <h3>
-            Sunrise: {sunrise} Sunset: {sunset}
-          </h3>
-          <img src={icon} alt="WeatherIcon"></img>
-          <p>{description}</p>
+      <div
+        className="card w-75 text-black"
+        style={{ backgroundColor: "yellow" }}
+      >
+        <div className="card-body p-3 row d-flex align-items-center">
+          <div className="col">
+            <div className="p-3">
+              <h2 className="card-title fw-bold">
+                {city.name} ({city.country})
+              </h2>
+              <h3 className="fw-bold">Current Temperature {currentTemp}°F</h3>
+            </div>
+            <div className="ps-3 row d-flex align-items-end ">
+              <h4 className="col-2 fs-3 fw-bold ">{high}°F</h4>
+              <h4 className="col-2 fw-light">{low}°F</h4>
+            </div>
+            <div className="row p-3">
+              <h5 className="col-2">
+                <b>Sunrise</b> <br />
+                {sunrise}
+              </h5>
+              <h5 className="col-2">
+                <b>Sunset</b> <br />
+                {sunset}
+              </h5>
+            </div>
+          </div>
+          <div className="col-3 d-flex align-items-center justify-content-center">
+            <div className="d-flex flex-column align-items-center justify-content-center">
+              <img
+                src={icon}
+                alt="Weather Icon"
+                className="align-middle"
+                style={{ width: "7rem", height: "7rem" }}
+              ></img>
+              <p className="fw-bold text-center">{description}</p>
+            </div>
+          </div>
         </div>
       </div>
     </>
